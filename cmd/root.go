6c8cac90ca7acf092757c51747f8c729d8148ce9
Peter Parada <parad.peter@gmail.com>
2021-05-22 17:19:54 +0200

    Remove unused code

--- cmd/root.go
+++ cmd/root.go
@@ -17,12 +17,7 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-	"os"
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -36,31 +31,4 @@ var rootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".grafana-simple-dashboard-generator" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".grafana-simple-dashboard-generator")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
