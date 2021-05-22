ad4edbbe49d09c0eabb64758a379aabd5734d79e
Peter Parada <parad.peter@gmail.com>
2021-05-22 17:20:22 +0200

    Remove unused code

--- cmd/root.go
+++ cmd/root.go
@@ -20,15 +20,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var cfgFile string
-
 var rootCmd = &cobra.Command{
 	Use:   "grafana-simple-dashboard-generator",
 	Short: "Generate simple grafana dashboard based on your needs",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
 }
